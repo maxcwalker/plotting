16d4c195aaf93fea6d7c382922d0c162a43578cd
Max Walker <maxwalker97@hotmail.co.uk>
2022-11-17 12:47:30 +0000

    ^

--- su2plot.py
+++ su2plot.py
@@ -1,27 +1,49 @@
+#This plotting script is for a standard compressible flow
+
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
-f = np.genfromtxt("./restart_flow.csv", names=True, delimiter = ',')
-x = f['x']
+f = np.genfromtxt("../SU2/mach6_comp_lam_plate/restart_flow.csv", names=True, delimiter = ',')
+x = np.around(f['x'], 5)
 y = f['y']
-rho = f['Density']
-rhou = f['Momentum_x']
-rhov = f['Momentum_y']
-E= f['Energy']
-P = f['Pressure']
-T = f['Temperature']
-ma = f['Mach']
-cp = f['Pressure_Coefficient']
-mu = f['Laminar_Viscosity']
 
+n = 8 # number of decimals to round values to
+rho = np.around(f['Density'], decimals=n)
+rhou = np.around(f['Momentum_x'], decimals=n)
+rhov = np.around(f['Momentum_y'], decimals=n)
+E= np.around(f['Energy'], decimals=n)
+P = np.around(f['Pressure'], decimals=n)
+T = np.around(f['Temperature'], decimals=n)
+ma = np.around(f['Mach'], decimals=n)
+cp = np.around(f['Pressure_Coefficient'], decimals=n)
+mu = np.around(f['Laminar_Viscosity'], decimals=n)
+
+# velocity from the momentum
 u = rhou/rho
 v = rhov/rho
 u_max = max(u)
 max_i = str(int((np.where(x == max(x))[0][0])/65))
-print("I must not exceed "+max_i)
-i = input("Choose a value of i:")
-i = int(i)
+#first attempt
+#print("I must not exceed "+max_i)
+#i = input("Choose a value of i:")
+#i = int(i)
+
+i = 0
+while x[i+1] == x[i]:
+    print(i)
+    if x[i+1] != x[i]:
+        break
+    i += 1
+
+j = 0
+if y[j] == 0:
+    j += 1
+print("the mesh is a {i} x {y} case")
+
+print({i})
+
 
 try:
     if i>1:
