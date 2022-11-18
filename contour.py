f2f23a47df38319535e8869a874082f499f09cfe
maxcwalker <maxwalker97@hotmail.co.uk>
2022-11-18 08:25:12 +0000

    EOD Mac 17/11

---
+++ contour.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm # latex module
+#import pandas as pd # may not need
+
+f = np.genfromtxt("restart_flow.csv", names = True, delimiter=',')
+
+#extracting the variables
+n = 5 # number of decimals to round to
+x = np.around(f['x'], decimals=n)
+y = np.around(f['y'], decimals=n)
+try:
+    rho = np.around(f['Density'], decimals=n)
+except:
+    rho1 = np.around(f['Density_0'], decimals=n)
+    rho2 = np.around(f['Density_1'], decimals=n)
+    rho3 = np.around(f['Density_2'], decimals=n)
+    rho4 = np.around(f['Density_3'], decimals=n)
+    rho5 = np.around(f['Density_4'], decimals=n)
+    rhon = [rho1,rho2,rho3,rho4,rho5]
+    rho = sum(rhon)
+
+rhou = np.around(f['Momentum_x'], decimals=n)
+rhov = np.around(f['Momentum_y'], decimals=n)
+E= np.around(f['Energy'], decimals=n)
+P = np.around(f['Pressure'], decimals=n)
+try:
+    T = np.around(f['Temperature'], decimals=n)
+except:
+    T = np.around(f['Temperature_tr'], decimals=n)
+ma = np.around(f['Mach'], decimals=n)
+cp = np.around(f['Pressure_Coefficient'], decimals=n)
+mu = np.around(f['Laminar_Viscosity'], decimals=n)
+# velocity from the momentum
+u = rhou/rho
+v = rhov/rho
+u_max = max(u)
+
+# ######################## #
+# ######################## #
+
+ax1 = plt.contourf(x, y, T)
+plt.show()
--- su2plot.py
+++ su2plot.py
@@ -2,78 +2,82 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import math 
 
-
-f = np.genfromtxt("../SU2/mach6_comp_lam_plate/restart_flow.csv", names=True, delimiter = ',')
-x = np.around(f['x'], 5)
-y = f['y']
-
+f = np.genfromtxt("../SU2/tutorials/compressible/lam_flatplate/restart_flow.csv", names=True, delimiter = ',')
 n = 8 # number of decimals to round values to
-rho = np.around(f['Density'], decimals=n)
+x = np.around(f['x'], 5)
+y = np.around(f['y'], 5)
+try:
+    rho = np.around(f['Density'], decimals=n)
+except:
+    rho1 = np.around(f['Density_0'], decimals=n)
+    rho2 = np.around(f['Density_1'], decimals=n)
+    rho3 = np.around(f['Density_2'], decimals=n)
+    rho4 = np.around(f['Density_3'], decimals=n)
+    rho5 = np.around(f['Density_4'], decimals=n)
+    rhon = [rho1,rho2,rho3,rho4,rho5]
+    rho = sum(rhon)
+
 rhou = np.around(f['Momentum_x'], decimals=n)
 rhov = np.around(f['Momentum_y'], decimals=n)
 E= np.around(f['Energy'], decimals=n)
 P = np.around(f['Pressure'], decimals=n)
-T = np.around(f['Temperature'], decimals=n)
+try:
+    T = np.around(f['Temperature'], decimals=n)
+except:
+    T = np.around(f['Temperature_tr'], decimals=n)
 ma = np.around(f['Mach'], decimals=n)
 cp = np.around(f['Pressure_Coefficient'], decimals=n)
 mu = np.around(f['Laminar_Viscosity'], decimals=n)
-
+print(rho)
 # velocity from the momentum
 u = rhou/rho
 v = rhov/rho
 u_max = max(u)
-max_i = str(int((np.where(x == max(x))[0][0])/65))
-#first attempt
-#print("I must not exceed "+max_i)
-#i = input("Choose a value of i:")
-#i = int(i)
+print(u)
 
+#working out the mesh dimensions
+#for x
 i = 0
+xg = 1 #x grid points
 while x[i+1] == x[i]:
-    print(i)
     if x[i+1] != x[i]:
         break
     i += 1
+    xg += 1
 
+#for y
 j = 0
-if y[j] == 0:
-    j += 1
-print("the mesh is a {i} x {y} case")
+yg = 0 #y grid points
+n = len(y)
+for j in range(n):
+    if y[j] == 0:
+        yg += 1
 
-print({i})
+#telling you the mesh dimensions used
+print("the mesh is a "+str(xg)+ "x" +str(yg)+" case")
 
+i = len(x) - i
+print("i is =" +str(i))
 
-try:
-    if i>1:
-        i = (i-1)*65+2
-    else:
-        i = 1
+pos = (x[i])
+pos = round(pos, 3)
+pos = str(pos)
 
-    print(" x ="+str(x[i]))
-    if x[i]<0:
-        print("This position is before the plate")
+new_var = y_pos= y[i:i+63]
+new_var
+#y_pos = np.flip(y_pos)
+u_x = u[i:i+63]
+u_x = u_x/u_max
+#u_x = np.flip(u_x)
 
-    pos = (x[i])
-    pos = round(pos, 3)
-    pos = str(pos)
-
-    new_var = y_pos= y[i:i+63]
-    new_var
-    #y_pos = np.flip(y_pos)
-    u_x = u[i:i+63]
-    u_x = u_x/u_max
-    #u_x = np.flip(u_x)
-
-    ax1 = plt.plot( u_x, y_pos)
-    plt.xlabel("u/$U_{inf}$")
-    plt.ylabel("normal position from the wall [m]")
-    plt.title("Velocity profile at point "+pos+"m along plate")
-    plt.xlim([0, 1])
-    plt.ylim([0,0.01])
-    plt.show()
-
-except:
-    print("A point has been chosen outside of the case domain")
-    print("Please choose a smaller value of i")
+#plotting the velocity profile
+ax1 = plt.plot( u_x, y_pos)
+plt.xlabel("u/$U_{inf}$")
+plt.ylabel("normal position from the wall [m]")
+plt.title("Velocity profile at point "+pos+"m along plate")
+plt.xlim([0, 1])
+plt.ylim([0,0.01])
+plt.show()
+exit()
